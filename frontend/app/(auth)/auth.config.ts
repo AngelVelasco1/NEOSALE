5289e97297e66b54402bda7ce3f6ddb266477c6c
Angel Velasco <angdvelasco@misena.edu.co>
2026-02-19 22:10:17 -0500

    feat: change cookies names auth

--- frontend/app/(auth)/auth.config.ts
+++ frontend/app/(auth)/auth.config.ts
@@ -146,4 +146,17 @@ export default {
       },
     }),
   ],
+  cookies: {
+    sessionToken: {
+      name: process.env.NODE_ENV === "production"
+        ? "__Secure-authjs.session-token"
+        : "authjs.session-token",
+      options: {
+        httpOnly: true,
+        sameSite: "lax",
+        path: "/",
+        secure: process.env.NODE_ENV === "production",
+      },
+    },
+  },
 } satisfies NextAuthConfig;
